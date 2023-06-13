85ce37afb8f74392d7f5489ca4024f82a8ddeca6
Erik Sleziona <erik.sleziona@gmail.com>
2023-06-13 15:08:58 +0200

    ...

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./global-styles/style.scss";
-
+import { AppProvider } from "./context";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <AppProvider>
+      <App />
+    </AppProvider>
   </React.StrictMode>
 );
--- src/modules/HomeModule/components/Form/Form.tsx
+++ src/modules/HomeModule/components/Form/Form.tsx
@@ -1,6 +1,10 @@
+import { useGlobalContext } from "@/context";
+import { AppContextType } from "@/context/Context.types";
 import React from "react";
 
 export const Form: React.FC = () => {
+  const { options } = useGlobalContext() as AppContextType;
+  console.log(options);
   return (
     <div className="form-container">
       <form className="form-container__form">
