c19e074212f8774cac01f0a7184f05602b90e472
Erik Sleziona <erik.sleziona@gmail.com>
2023-06-19 13:51:14 +0200

    Adding Copy  Function, toast if succes, Bug fix. when all options was off the  we was faling in some infinity loop. Showing some error toast to the User

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./global-styles/style.scss";
 import { AppProvider } from "./context";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <ToastContainer position="top-center" />
     <AppProvider>
       <App />
     </AppProvider>
--- src/modules/HomeModule/components/Button/Button.tsx
+++ src/modules/HomeModule/components/Button/Button.tsx
@@ -1,9 +1,24 @@
 import React from "react";
 import "./Button.style.scss";
 import { LuCopy } from "react-icons/lu";
+import { useGlobalContext } from "@/context";
+import { AppContextType } from "@/context/Context.types";
+import { toast } from "react-toastify";
+
 export const Button: React.FC = () => {
+  const { password } = useGlobalContext() as AppContextType;
+
+  const handleClick = () => {
+    try {
+      navigator.clipboard.writeText(password);
+      toast.success("Password Copied !");
+    } catch (error) {
+      toast.error("Error");
+    }
+  };
+
   return (
-    <button type="button" className="btn">
+    <button type="button" className="btn" onClick={handleClick}>
       <LuCopy />
       <span className="btn__text">Copy Password</span>
     </button>
